ed11c3f20870719c6430fc648baf2effef2cc3f4
emmachinko <emmachris4603@gmail.com>
2024-03-02 01:25:32 -0800

    C - Binary trees

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -13,8 +13,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	}
 	else
 	{
-		func(tree->n);
 		binary_tree_inorder(tree->left, func);
+		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,8 +13,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	}
 	else
 	{
-		func(tree->n);
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
 	}
+	func(tree->n);
 }
